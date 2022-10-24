2cf607038fafe0031e7f04d884563c06363b002d
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2022-10-24 00:29:44 -0300

    Update graficos_media.py

--- graficos_media.py
+++ graficos_media.py
@@ -29,7 +29,9 @@ concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV
 Elementos=[Ac_228A1,keV911A1,Bi_212A1,Pb_212A1,Pb_214A1,Bi_214A1,keV1120A1,K_40A1] #EIXO Y
 #------------PLOT----------------
 fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A1"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_amostras.png"); 
+fig.close('all');
 #------------------------------------------------------------------------------------------
+
 
 #--------------------------------GRAFICOS CONCENTRACAO MEDIA A2--------------------------------
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
@@ -48,6 +50,7 @@ concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV
 Elementos=[Ac_228A2,keV911A2,Bi_212A2,Pb_212A2,Pb_214A2,Bi_214A2,keV1120A2,K_40A2] #EIXO Y
 #------------PLOT----------------
 fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A2"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A1.png"); 
+fig.close('all');
 #------------------------------------------------------------------------------------------
 
 #--------------------------------GRAFICOS CONCENTRACAO MEDIA A3--------------------------------
@@ -67,6 +70,7 @@ concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV
 Elementos=[Ac_228A3,keV911A3,Bi_212A3,Pb_212A3,Pb_214A3,Bi_214A3,keV1120A3,K_40A3] #EIXO Y
 #------------PLOT----------------
 fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A3"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A2.png"); 
+fig.close('all');
 #------------------------------------------------------------------------------------------
 
 #--------------------------------GRAFICOS CONCENTRACAO MEDIA AS--------------------------------
@@ -86,9 +90,13 @@ concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV
 Elementos=[Ac_228AS,keV911AS,Bi_212AS,Pb_212AS,Pb_214AS,Bi_214AS,keV1120AS,K_40AS] #EIXO Y
 #------------PLOT----------------
 fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A3.png"); 
+fig.close('all');
 #------------------------------------------------------------------------------------------
 
 #--------------------------------GRAFICOS CONCENTRACAO MEDIA K-40--------------------------------
+#____________EIXOS____________
 k40=(K_40A1, K_40A2, K_40A3, K_40AS, 400)
-Elementos=("A1", "A2", "A3", "AS" "Media mundial") #EIXO X
-fig.bar(Elementos, k40,); fig.grid(True); fig.title("Concetração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("comparacao_concentracao_k40png"); 
+Elementos=("A1", "A2", "A3", "AS" "Media mundial") 
+#------------PLOT----------------
+fig.bar(Elementos, k40); fig.grid(True); fig.title("Concetração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("comparacao_concentracao_k40png"); 
+fig.close('all');
