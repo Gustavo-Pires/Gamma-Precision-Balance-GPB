3d2a4e4a6d15ad0e6c93712d534324a406992181
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2022-11-01 00:39:55 -0300

    Update graficos_media.py
    Signed-off-by: Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>

--- graficos_media.py
+++ graficos_media.py
@@ -16,19 +16,22 @@ from typing import Union
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
 ws2 = xw.Book("contas.xlsx").sheets['Planilha1'] 
 #____________elementos____________
-Ac_228A1= ws2.range("AK61").value		
-keV911A1=ws2.range("AK64").value 			
-Bi_212A1=ws2.range("AK67").value	
-Pb_212A1=ws2.range("AK70").value	
-Pb_214A1=ws2.range("AK76").value	
-Bi_214A1=ws2.range("AK79").value
-keV1120A1=ws2.range("AK82").value			
-K_40A1=ws2.range("AK88").value 		
+Ac_228_keV338_A1= ws2.range("AK61").value		
+Ac_228_keV911_A1=ws2.range("AK64").value 
+Ac_228_media_A1=((float(Ac_228_keV338_A1)+float(Ac_228_keV911_A1))/2)
+Bi_212_A1=ws2.range("AK67").value	
+Pb_212_A1=ws2.range("AK70").value	
+Pb_214_A1=ws2.range("AK76").value	
+Bi_214_keV609_A1=ws2.range("AK79").value
+Bi_214_keV1120_A1=ws2.range("AK82").value
+Bi_212_media_A1=(float(Bi_214_keV609_A1)+float(Bi_214_keV1120_A1)/2)			
+K_40_A1=ws2.range("AK88").value 		
+
 #____________EIXOS____________
-concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV-1120", "K-40") #EIXO X
-Elementos=[Ac_228A1,keV911A1,Bi_212A1,Pb_212A1,Pb_214A1,Bi_214A1,keV1120A1,K_40A1] #EIXO Y
+concetracao=("Ac-228", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "K-40") #EIXO X
+Elementos=[Ac_228_media_A1, Bi_212_media_A1, Pb_212_A1, Pb_214_A1, Bi_214_keV609_A1, K_40_A1] #EIXO Y
 #------------PLOT----------------
-fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A1"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_amostras.png"); 
+fig.bar(concetracao, Elementos ); fig.grid(False); fig.title("Concentração média-A1"); fig.xlabel("Elemento"); fig.ylabel("Concentração(Bq/kg)"); fig.savefig("concentracao_A1.png"); 
 fig.close('all');
 #------------------------------------------------------------------------------------------
 
@@ -37,19 +40,20 @@ fig.close('all');
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
 ws2 = xw.Book("contas.xlsx").sheets['Planilha1'] 
 #____________elementos____________
-Ac_228A2= ws2.range("AL61").value		
-keV911A2=ws2.range("AL64").value 			
-Bi_212A2=ws2.range("AL67").value	
-Pb_212A2=ws2.range("AL70").value	
-Pb_214A2=ws2.range("AL76").value	
-Bi_214A2=ws2.range("AL79").value
-keV1120A2=ws2.range("AL82").value			
-K_40A2=ws2.range("AL88").value 		
+Ac_228_kev338_A2= ws2.range("AL61").value		
+Ac_228_keV911_A2=ws2.range("AL64").value
+Ac_228_media_A2=((float(Ac_228_kev338_A2)+float(Ac_228_keV911_A2))/2) 			
+Bi_212_A2=ws2.range("AL67").value	
+Pb_212_A2=ws2.range("AL70").value	
+Pb_214_A2=ws2.range("AL76").value	
+Bi_214_kev609_A2=ws2.range("AL79").value
+Bi_214_keV1120_A2=ws2.range("AL82").value	
+Bi_214_media_A2=((float(Bi_214_kev609_A2)+float(Bi_214_keV1120_A2))/2)		
+K_40_A2=ws2.range("AL88").value 		
 #____________EIXOS____________
-concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV-1120", "K-40") #EIXO X
-Elementos=[Ac_228A2,keV911A2,Bi_212A2,Pb_212A2,Pb_214A2,Bi_214A2,keV1120A2,K_40A2] #EIXO Y
+Elementos=[Ac_228_media_A2,Bi_212_A2,Pb_212_A2,Pb_214_A2,Bi_214_media_A2,K_40_A2] #EIXO Y
 #------------PLOT----------------
-fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A2"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A1.png"); 
+fig.bar(concetracao, Elementos ); fig.grid(False); fig.title("Concentração média-A2"); fig.xlabel("Elemento"); fig.ylabel("Concentração(Bq/kg)"); fig.savefig("concentracao_A2.png"); 
 fig.close('all');
 #------------------------------------------------------------------------------------------
 
@@ -57,19 +61,21 @@ fig.close('all');
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
 ws2 = xw.Book("contas.xlsx").sheets['Planilha1'] 
 #____________elementos____________
-Ac_228A3= ws2.range("AM61").value		
-keV911A3=ws2.range("AM64").value 			
-Bi_212A3=ws2.range("AM67").value	
-Pb_212A3=ws2.range("AM70").value	
-Pb_214A3=ws2.range("AM76").value	
-Bi_214A3=ws2.range("AM79").value
-keV1120A3=ws2.range("AM82").value			
-K_40A3=ws2.range("AM88").value 		
+Ac_228_kev338_A3= ws2.range("AM61").value		
+Ac_228_keV911_A3=ws2.range("AM64").value 
+Ac_228_keV911_A3=float(Ac_228_keV911_A3)
+Ac_228_media_A3=((float(Ac_228_kev338_A3)+float(Ac_228_keV911_A3))/2) 	
+Bi_212_A3=ws2.range("AM67").value	
+Pb_212_A3=ws2.range("AM70").value	
+Pb_214_A3=ws2.range("AM76").value	
+Bi_214_keV609_A3=ws2.range("AM79").value
+Bi_214_keV1120_A3=ws2.range("AM82").value	
+Bi_214_media_A3=((float(Bi_214_keV609_A3)+float(Bi_214_keV1120_A3))/2)		
+K_40_A3=ws2.range("AM88").value 		
 #____________EIXOS____________
-concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV-1120", "K-40") #EIXO X
-Elementos=[Ac_228A3,keV911A3,Bi_212A3,Pb_212A3,Pb_214A3,Bi_214A3,keV1120A3,K_40A3] #EIXO Y
+Elementos=[Ac_228_media_A3,Bi_212_A3,Pb_212_A3,Pb_214_A3,Bi_214_media_A3,K_40_A3] #EIXO Y
 #------------PLOT----------------
-fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-A3"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A2.png"); 
+fig.bar(concetracao, Elementos ); fig.grid(False); fig.title("Concentração média-A3"); fig.xlabel("Elemento"); fig.ylabel("Concentração(Bq/kg)"); fig.savefig("concentracao_A3.png"); 
 fig.close('all');
 #------------------------------------------------------------------------------------------
 
@@ -77,26 +83,27 @@ fig.close('all');
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
 ws2 = xw.Book("contas.xlsx").sheets['Planilha1'] 
 #____________elementos____________
-Ac_228AS= ws2.range("AN61").value		
-keV911AS=ws2.range("AN64").value 			
-Bi_212AS=ws2.range("AN67").value	
-Pb_212AS=ws2.range("AN70").value	
-Pb_214AS=ws2.range("AN76").value	
-Bi_214AS=ws2.range("AN79").value
-keV1120AS=ws2.range("AN82").value			
-K_40AS=ws2.range("AN88").value 		
+Ac_228_kev338_AS= ws2.range("AN61").value
+Ac_228_keV911_AS=ws2.range("AN64").value 	
+Ac_228_media_AS=((float(Ac_228_kev338_AS)+float(Ac_228_keV911_AS))/2)		
+Bi_212_AS=ws2.range("AN67").value	
+Pb_212_AS=ws2.range("AN70").value	
+Pb_214_AS=ws2.range("AN76").value	
+Bi_214_keV1120_AS=ws2.range("AN79").value
+Bi_214_keV1120_AS=ws2.range("AN82").value	
+Bi_214_media_AS=((float(Bi_214_keV1120_AS)+float(Bi_214_keV1120_AS)/2))		
+K_40_AS=ws2.range("AN88").value 		
 #____________EIXOS____________
-concetracao=("Ac-228", "ekeV-911", "Bi-212", "Pb-212", "Pb-214", "Bi-214", "ekeV-1120", "K-40") #EIXO X
-Elementos=[Ac_228AS,keV911AS,Bi_212AS,Pb_212AS,Pb_214AS,Bi_214AS,keV1120AS,K_40AS] #EIXO Y
+Elementos=[Ac_228_media_AS,Bi_212_AS,Pb_212_AS,Pb_214_AS,Bi_214_media_AS,K_40_AS] #EIXO Y
 #------------PLOT----------------
-fig.bar(concetracao, Elementos ); fig.grid(True); fig.title("Concetração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("concentracao_A3.png"); 
+fig.bar(concetracao, Elementos ); fig.grid(False); fig.title("Concentração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concentração(Bq/kg)"); fig.savefig("concentracao_AS.png"); 
 fig.close('all');
 #------------------------------------------------------------------------------------------
 
 #--------------------------------GRAFICOS CONCENTRACAO MEDIA K-40--------------------------------
 #____________EIXOS____________
-k40=(K_40A1, K_40A2, K_40A3, K_40AS, 400)
-Elementos=("A1", "A2", "A3", "AS" "Media mundial") 
+k40=(K_40_A1, K_40_A2, K_40_A3, K_40_AS, 400) 
+Elementos=("A1", "A2", "A3", "AS", "Media mundial") 
 #------------PLOT----------------
-fig.bar(Elementos, k40); fig.grid(True); fig.title("Concetração média-AS"); fig.xlabel("Elemento"); fig.ylabel("Concetração(Bq/kg)"); fig.savefig("comparacao_concentracao_k40png"); 
-fig.close('all');
+fig.bar( Elementos, k40); fig.grid(False); fig.title("Concentração média Potassio-40"); fig.xlabel("Elemento"); fig.ylabel("Concentração(Bq/kg)"); fig.savefig("comparacao_concentracao_k40png"); 
+fig.close('all'); 
