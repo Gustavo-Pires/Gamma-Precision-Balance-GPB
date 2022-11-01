9976ef9dee8e7e4600309bf659a7403c107d739e
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2022-11-01 02:23:30 -0300

    Update contas.py
    Signed-off-by: Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>

--- contas.py
+++ contas.py
@@ -13,32 +13,52 @@ from statistics import median
 from math import isnan
 from itertools import filterfalse
 
+def clearzero():
+    for index, value in enumerate(listadef):
+        if value <0:
+            negativo=(index(value)
+            p_40_coluna_concentracao.remove(negativo)
+            p_40_coluna_incerteza.remove(negativo)
+            
+def calculo_media():
+    def somalista():
+        soma = 0
+        for i in media:
+            soma = soma + i
+        return soma
+    def fazendomedia():
+        media=((soma)/len(media))
+
+media=()
+
+
 #-----ABRINDO O ARQUIVO DE CONTAS----------------
 ws2 = xw.Book("contas.xlsx").sheets["Dados brutos"] 
 
-#_________________________________Potassio 40_________________________________
+#_________________________________Potassio 40__________________________________
 p_40_coluna_concentracao = ws2.range("AD32:AD70").value
 p_40_coluna_incerteza = ws2.range("AE32:AE70").value
 med_mundial_p40=((400))
 
+clearzero() #para retirar os negativos
 
-for index, value in enumerate(p_40_coluna_concentracao):
-    if value <0:
-        print(index)
-        remover=(index)
-        p_40_coluna_concentracao.remove(value)
-        p_40_coluna_incerteza.remove(remover)
+media=p_40_coluna_concentracao
+med_def_con_p40= calculo_media()
+
+media=p_40_coluna_incerteza
+med_def_inc_p40=calculo_media() 
+#______________________________________________________________________________
 
 
-def somalista(numeros):
-    soma = 0
-    for i in numeros:
-        soma = soma + i
-    return soma
-
-media_concentracao_def=((somalista(p_40_coluna_concentracao))/len(p_40_coluna_incerteza )
-media_concentracao_def=((somalista(p_40_coluna_concentracao))/len(p_40_coluna_incerteza )
+#media_concentracao_def=((somalista(p_40_coluna_concentracao))/len(p_40_coluna_incerteza )            
+#media_concentracao_def=((somalista(p_40_coluna_concentracao))/len(p_40_coluna_incerteza )
         
 #-----------MEDIAS----------
-p_40_med_concentracao = statistics.mean(p_40_coluna_concentracao)
-p_40_med_incerteza=statistics.mean(p_40_coluna_incerteza)
+p_40_med_= statistics.mean(p_40_coluna_concentracao)
+p_40_incerteza=statistics.mean(p_40_coluna_incerteza)
+
+print("A media concetracao pela funcao que soma a lista é:", ed_def_con_p40)   #------------------FUNCAO DEF
+print("A media incerteza pela funcao que soma a lista é:", med_def_inc_p40)
+
+print("A media concentracao pela statistics é:", p_40_med)                     #------------------statistics
+print("A media incerteza pela fstatistics é:", p_40_incerteza)
