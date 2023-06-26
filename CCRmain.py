a872eea5aa6143f64b9ce619a04f498134367420
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2023-06-25 22:51:02 -0300

    Update CCRmain.py
    Signed-off-by: Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>

--- CCRmain.py
+++ CCRmain.py
@@ -7,7 +7,7 @@
     
 #-------------IMPORTANTE--------------------------------
 #Uma pasta do Windows: C:\Windows\Jose\Python
-#Um diretório no Linux : /home/user/Maria/Python
+#Um diretório no Linux : /home/user/Maria/Python  
 #Ou seja, as barras são invertidas!
 #Se tentar acessar um endereço que não existe (como um C: no Linux ou /home no Windows) vai dar erro sim em seus scripts!
 #-------------------------------------------------------
