cf07d4811baf693d5c983d779fd7e91982c0efb7
Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>
2023-07-28 16:40:31 -0300

    Update GPB v1.0-alpha.py
    Arquivo de teste para rodar todos os dados de um mes de forma automatica 
    
    Signed-off-by: Gustavo Pires Bertaco <85207790+Gustavo-Pires@users.noreply.github.com>

--- Teste Artigo/GPB v1.0-alpha.py
+++ Teste Artigo/GPB v1.0-alpha.py
@@ -1,110 +1,189 @@
-import time
-start_time = time.time()  # Captura o tempo de início
-import xlwings as xw
-import matplotlib.pyplot as plt
-from datetime import datetime
-import matplotlib.dates as mdates
-import os
-import glob
+#---------------------------------------------CONTAGEM -------------------------------------
+print("=" * 20 + "CALIBRAÇÃO INICIADA" + "=" * 20)
 
-diretorio_atual = os.path.abspath(os.path.dirname(__file__))  # Obter o caminho absoluto do diretório atual
+diretorio_principal = os.getcwd() # usa o diretório atual como o diretório principal
 
-arquivos_excel = glob.glob(os.path.join(diretorio_atual, '*.xls'))  # Obter a lista de arquivos Excel no diretório
+# Abre o arquivo de log em modo de anexação
+with open("log_erros.txt", "a") as log_file:
+    for diretorio_atual, _, arquivos in os.walk(diretorio_principal):
+        arquivos_csv = [f for f in arquivos if f.endswith('.csv')]
 
-def plotar_grafico(ekev_57, ekev_60, dia, titulo, media_57, media_60, incerteza_57, incerteza_60):
-    fig, axs = plt.subplots(2, 2, figsize=(12, 8))
+        if len(arquivos_csv) == 0:
+            mensagem = f"Nenhum arquivo CSV foi encontrado no diretório {diretorio_atual}."
+            print(mensagem)
+            log_file.write(mensagem + "\n")
+            continue
+        elif len(arquivos_csv) > 1:
+            mensagem = f"Mais de um arquivo CSV foi encontrado no diretório {diretorio_atual}. \nDeixe apenas o arquivo correto"
+            print(mensagem)
+            log_file.write(mensagem + "\n")
+            continue
 
-    ax1, ax2, ax3, ax4 = axs.flatten()
+        arquivo_csv = os.path.join(diretorio_atual, arquivos_csv[0])
+        try:
+            with open(arquivo_csv, newline='') as file:
+                lines = file.readlines()
 
-    ax1.scatter(dia, ekev_57, color='blue', alpha=0.5, label='Cobalto-57 (122,06 keV)')
-    ax1.set_title('Cobalto-57 (122,06 keV)')
-    ax1.set_xlabel('Dia')
-    ax1.set_ylabel('Energia(keV)')
-    ax1.axhline(y=124.06, color='red', linestyle='--', label='124.06')
-    ax1.axhline(y=120.06, color='green', linestyle='--', label='120.06')
-    ax1.set_ylim(117.06, 127.06)
+                # Extrair as colunas específicas
+                ekev = [str(line.split(';')[0]).replace(',', '.') for line in lines[6:27]]
+                resolucao = [str(line.split(';')[1]).replace(',', '.') for line in lines[6:27]]
+                canal = [str(line.split(';')[5]).replace(',', '.') for line in lines[6:27]]
+                contagem = [str(line.split(';')[3]).replace(',', '.') for line in lines[6:27]]
+                incerteza = [str(line.split(';')[4]).replace(',', '.') for line in lines[6:27]]
+                data = str(lines[1].split(';')[0])
+                data_hora = str(lines[1].split(';')[0])
+                nome_amostra = str(lines[0].split(';')[0].rsplit("\\", 1)[-1])
+                print(nome_amostra)
 
-    ax2.scatter(dia, reso_57, color='blue', alpha=0.5, label='Cobalto-57 (122,06 keV)')
-    ax2.set_title('Cobalto-57 (122,06 keV)')
-    ax2.set_xlabel('Dia')
-    ax2.set_ylabel('Resolucao')
-    ax2.set_ylim(0.5, 2)  # Definindo os limites do eixo Y para o Co-57
-    ax2.axhline(y=media_57)
-    ax2.axhline(y=media_57 + incerteza_57, color='red', linestyle='--', label='Maior Incerteza')
-    ax2.axhline(y=media_57 - incerteza_57, color='green', linestyle='--', label='Menor Incerteza')
+                # Remover células vazias
+                ekev = [x if x != 'None' else '' for x in ekev]
+                resolucao = [x if x != 'None' else '' for x in resolucao]
+                canal = [x if x != 'None' else '' for x in canal]
+                contagem = [x if x != 'None' else '' for x in contagem]
+                incerteza = [x if x != 'None' else '' for x in incerteza]
 
+                # Mover o arquivo CSV para a pasta "ARQUIVO"
+                pasta_destino = os.path.join(diretorio_atual, 'ARQUIVO')
+                if not os.path.exists(pasta_destino):
+                    os.makedirs(pasta_destino)
+                
+                nome_arquivo = os.path.basename(arquivo_csv)
+                caminho_destino = os.path.join(pasta_destino, nome_arquivo)
+                
+                shutil.move(arquivo_csv, caminho_destino)
+                print
+        except FileNotFoundError:
+            mensagem = "Arquivo CSV não encontrado."
+            print(mensagem)
+            log_file.write(mensagem + "\n")
+#------------------------------------------------------------------------------------------------------
+end_time1= time.time()
 
-    ax3.scatter(dia, ekev_60, color='blue', alpha=0.5, label='Cobalto-60 (1332,5 keV)')
-    ax3.set_title('Cobalto-60 (1332,5 keV)')
-    ax3.set_xlabel('Dia')
-    ax3.set_ylabel('Energia(keV)')
-    ax3.axhline(y=1330.5, color='red', linestyle='--', label='1330.5')
-    ax3.axhline(y=1334.5, color='green', linestyle='--', label='1334.5')
-    ax3.set_ylim(1327.5, 1337.5)
+elapsed_time1= end_time1 - start_time1
+nome = input('Digite seu nome: ')
+start_time2= time.time() 
+#---------------------------------------------ARQUIVO DE CONTAGEM -------------------------------------
+
+#---------------------------------------------IMPORTANDO DADOS-------------------------------------
+ws2 = xw.Book("Calibracao.xlsx").sheets['Calibracao']
+
+data_hora= ws2.range("B9:B40").options(numbers=str).value
+
+co_57_ekev= ws2.range("C9:C40").options(numbers=str).value
+co_57_resolucao= ws2.range("D9:D40").options(numbers=str).value
+co_57_canal =ws2.range("E9:E40").options(numbers=str).value
+co_57_contagem =ws2.range("F9:F40").options(numbers=str).value
+co_57_incerteza =ws2.range("G9:G40").options(numbers=str).value
+
+co_60_ekev= ws2.range("H9:H40").options(numbers=str).value
+co_60_resolucao= ws2.range("I9:I40").options(numbers=str).value
+co_60_canal =ws2.range("J9:J40").options(numbers=str).value
+co_60_contagem =ws2.range("K9:K40").options(numbers=str).value
+co_60_incerteza =ws2.range("L9:L40").options(numbers=str).value
+
+usuario= ws2.range("M9:M40").options(numbers=str).value
+#------------------------------------------------------------------------------------------------------
+
+#----------removendo celulas vazias----------
+data_hora=[x for x in data_hora if x is not None]
+
+co_57_ekev= [x for x in co_57_ekev if x is not None]
+co_57_resolucao= [x for x in co_57_resolucao if x is not None]
+co_57_canal =[x for x in co_57_canal if x is not None]
+co_57_contagem =[x for x in co_57_contagem if x is not None]
+co_57_incerteza =[x for x in co_57_incerteza if x is not None]
+
+co_60_ekev= [x for x in co_60_ekev if x is not None]
+co_60_resolucao= [x for x in co_60_resolucao if x is not None]
+co_60_canal =[x for x in co_60_canal if x is not None]
+co_60_contagem =[x for x in co_60_contagem if x is not None]
+co_60_incerteza=[x for x in co_60_incerteza if x is not None]
+
+usuario= [x for x in usuario if x is not None]
+#----------------------------------------------
+
+data_hora.append(data)
+usuario.append(nome)
+
+#-----------------------------------------ACHANDO OS PICOS-------------------------------------------------------------
+valor_procurado = 122.06
+encontrado = False
+
+for j in range(len(ekev)):
+    valor = ekev[j].replace(" ", "").replace(",", ".")
+    if float(valor) >= valor_procurado - 2 and float(valor) <= valor_procurado + 2:
+        encontrado = True
+        co_57_ekev.append(ekev[j])
+        co_57_resolucao.append(resolucao[j])
+        co_57_contagem.append(contagem[j])
+        co_57_incerteza.append(incerteza[j])
+        co_57_canal.append(canal[j])
+        break  # interrompe o laço de repetição
+
+if not encontrado:
+    print("Pico de energia do co-57 não encontrado dentro da variação.")
+    print("="*20 + "CALIBRAÇÃO NÃo CONCLUIDA" + "="*20  )
+    sys.exit()
+
     
-    ax4.scatter(dia, reso_60, color='blue', alpha=0.5, label='Cobalto-60 (1332,5 keV)')
-    ax4.set_title('Cobalto-60 (1332,5 keV)')
-    ax4.set_xlabel('Dia')
-    ax4.set_ylabel('Resolucao')
-    ax4.set_ylim(1, 3)  # Definindo os limites do eixo Y para o Co-60
-    ax4.axhline(y=media_60)
-    ax4.axhline(y=media_60 + incerteza_60, color='red', linestyle='--', label='Maior Incerteza')
-    ax4.axhline(y=media_60 - incerteza_60, color='green', linestyle='--', label='Menor Incerteza')
+valor_procurado = 1332.5
+encontrado = False
 
-    fig.suptitle(nome_arquivo)
+for j in range(len(ekev)):
+    valor = ekev[j].replace(" ", "").replace(",", ".")
+    if float(valor) >= valor_procurado - 2 and float(valor) <= valor_procurado + 2:
+        encontrado = True
+        co_60_ekev.append(ekev[j])
+        co_60_resolucao.append(resolucao[j])
+        co_60_contagem.append(contagem[j])
+        co_60_incerteza.append(incerteza[j])
+        co_60_canal.append(canal[j])
+        break  # interrompe o laço de repetição
 
-    ax1.xaxis.set_major_locator(mdates.DayLocator())
-    ax2.xaxis.set_major_locator(mdates.DayLocator())
-    ax3.xaxis.set_major_locator(mdates.DayLocator())
-    ax4.xaxis.set_major_locator(mdates.DayLocator())
+if not encontrado:
+    print("Pico de energia do co-60 não encontrado dentro da variação.")
+    print("="*20 + "CALIBRAÇÃO NÃo CONCLUIDA" + "="*20  )
+    sys.exit()
+#----------------------------------------------------------------------------------------------------------------
 
-    plt.tight_layout()
-    plt.savefig(f'{nome_arquivo}.png', dpi=300)
-    wb.close() 
+#-----------------------SALVANDO-----------------------------------------------------------------------------------------
+# Convertendo os valores para o formato americano com ponto decimal
+co_57_ekev = [x.replace('.', ',') for x in co_57_ekev]
+co_57_resolucao = [x.replace('.', ',') for x in co_57_resolucao]
+co_57_canal = [x.replace('.', ',') for x in co_57_canal]
+co_57_contagem = [x.replace('.', ',') for x in co_57_contagem]
+co_57_incerteza = [x.replace('.', ',') for x in co_57_incerteza]
 
-total_dias = [] 
+co_60_ekev = [x.replace('.', ',') for x in co_60_ekev]
+co_60_resolucao = [x.replace('.', ',') for x in co_60_resolucao]
+co_60_canal = [x.replace('.', ',') for x in co_60_canal]
+co_60_contagem = [x.replace('.', ',') for x in co_60_contagem]
+co_60_incerteza = [x.replace('.', ',') for x in co_60_incerteza]
 
-for arquivo_excel in arquivos_excel:
-    print(arquivo_excel)
-    wb = xw.Book(arquivo_excel)
-    ws = wb.sheets['Plan1']
+# Salvando os dados convertidos no formato americano
+ws2.range('B9:B40').options(transpose=True).value = [data_hora]
+ws2.range('C9:C40').options(transpose=True).value = [co_57_ekev]
+ws2.range('D9:D40').options(transpose=True).value = [co_57_resolucao]
+ws2.range('E9:E40').options(transpose=True).value = [co_57_canal]
+ws2.range('F9:F40').options(transpose=True).value = [co_57_contagem]
+ws2.range('G9:G40').options(transpose=True).value = [co_57_incerteza]
+ws2.range('H9:H40').options(transpose=True).value = [co_60_ekev]
+ws2.range('I9:I40').options(transpose=True).value = [co_60_resolucao]
+ws2.range('J9:J40').options(transpose=True).value = [co_60_canal]
+ws2.range('K9:K40').options(transpose=True).value = [co_60_contagem]
+ws2.range('L9:L40').options(transpose=True).value = [co_60_incerteza]
+ws2.range('M9:M40').options(transpose=True).value = [usuario]
+#----------------------------------------------------------------------------------------------------------------
 
-    ekev_57 = ws.range("C9:C40").options(numbers=str).value
-    ekev_60 = ws.range("H9:H40").options(numbers=str).value
-    reso_57 = ws.range("D9:D40").options(numbers=str).value
-    reso_60 = ws.range("I9:I40").options(numbers=str).value
-    media_57 = ws.range("D42").options(numbers=float).value
-    media_60 = ws.range("I42").options(numbers=float).value
+#-----------------------------------------------------------------
+end_time2= time.time()
+elapsed_time2= end_time2 - start_time2
+elapsed_time = elapsed_time1 + elapsed_time2
+#-----------------------------------------------------------------
 
-    dia = ws.range("B9:B40").options(numbers=str).value
-    dia = [x.strftime('%d') if isinstance(x, datetime) else x for x in dia if x is not None]
-    total_dias.extend(dia)  # Adiciona os dias � lista total_dias
-  
-    nome_arquivo = os.path.splitext(os.path.basename(arquivo_excel))[0]  # Obter o nome do arquivo sem a extens�o
-    titulo_completo = f"{nome_arquivo}"
+#wb.save()
+#ws2.close()
 
-    ekev_57 = [float(x) for x in ekev_57 if x is not None and x.strip() != '']
-    ekev_60 = [float(x) for x in ekev_60 if x is not None and x.strip() != '']
-    reso_57 = [float(x) for x in reso_57 if x is not None and x.strip() != '']
-    reso_60 = [float(x) for x in reso_60 if x is not None and x.strip() != '']
-
-    incerteza_57 = 0.2 * media_57  # 20% da m�dia de incerteza para o Cobalto-57
-    incerteza_60 = 0.3 * media_60  # 30% da m�dia de incerteza para o Cobalto-60
-
-    plotar_grafico(ekev_57, ekev_60, dia, titulo_completo, media_57, media_60, incerteza_57, incerteza_60)
-
-end_time = time.time()  
-elapsed_time = end_time - start_time  
-
-num_arquivos_excel = len(arquivos_excel)
-num_dias_calibracao = len(total_dias)
-
-# Exibe os resultados
-print(f"Tempo decorrido: {elapsed_time:.2f} segundos")
-print(f"Numero de arquivos Excel analisados: {num_arquivos_excel}")
-print(f"Numero de dias de calibracao plotados: {num_dias_calibracao}")
-
-
-
-
+print("Tempo de execução:", elapsed_time, "segundos")
+print("="*20 + "CALIBRAÇÃO CONCLUIDA" + "="*20  )
 
